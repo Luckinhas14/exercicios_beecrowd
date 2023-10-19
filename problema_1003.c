1fcfef9b9d0aa97657a383def2fc76e784736229
luckinhas14 <lucas.lunarde@gmail.com>
2023-10-18 22:49:41 -0300

    resolução do problema 1003

---
+++ problema_1003.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+ 
+    int A,B,SOMA;
+
+   printf("Digite um numero: ");
+    scanf("%d",&A);
+   printf("Digite um numero: ");
+    scanf("%d",&B);
+    
+    SOMA = A+B;
+    
+    printf("SOMA = %d\n",SOMA);
+ 
+    return 0;
+}
